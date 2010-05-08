ecd8090a2f7cec3ae97a2f3dea9fea6091862414
hernani <hernani@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-05-08 18:05:52 +0000

    Fix in sitemap.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@179 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum/sitemap.py
+++ forum/sitemap.py
@@ -5,7 +5,7 @@ class QuestionsSitemap(Sitemap):
     changefreq = 'daily'
     priority = 0.5
     def items(self):
-        return Question.objects.exclude(deleted=True)
+        return Question.objects.filter(deleted=None)
 
     def lastmod(self, obj):
         return obj.last_activity_at
