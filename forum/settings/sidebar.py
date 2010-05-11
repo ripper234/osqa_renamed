2017cbd3e796117e1fdf57181d7b7443f8696657
rick <rick@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-05-11 02:44:51 +0000

    Minor tweaks to default user sidebar content wording and block display order.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@211 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum/settings/sidebar.py
+++ forum/settings/sidebar.py
@@ -3,7 +3,7 @@ from django.forms.widgets import Textarea
 
 SIDEBAR_SET = SettingSet('sidebar', 'Sidebar content', "Enter contents to display in the sidebar. You can use markdown and some basic html tags.", 10, True)
 
-SIDEBAR_UPPER_SHOW = Setting('SIDEBAR_UPPER_SHOW', False, SIDEBAR_SET, dict(
+SIDEBAR_UPPER_SHOW = Setting('SIDEBAR_UPPER_SHOW', True, SIDEBAR_SET, dict(
 label = "Show Upper Block",
 help_text = "Check if your pages should display the upper sidebar block.",
 required=False))
@@ -13,15 +13,16 @@ SIDEBAR_UPPER_TEXT = Setting('SIDEBAR_UPPER_TEXT',
 u"""
 ## [Try WebFaction](http://www.webfaction.com?affiliate=osqa)
 
-We recommend [**WebFaction**](http://www.webfaction.com?affiliate=osqa)
-for hosting OSQA. Their affordable, reliable servers have everything you need!
+We recommend [**WebFaction**](http://www.webfaction.com?affiliate=osqa) \
+for hosting OSQA. Their affordable, reliable servers have everything you need! \
+[Step-by-step instructions...](http://wiki.osqa.net/display/docs/Installing+OSQA+on+WebFaction)
 """, SIDEBAR_SET, dict(
 label = "Upper Block Content",
 help_text = " The upper sidebar block. ",
 widget=Textarea(attrs={'rows': '10'})))
 
 
-SIDEBAR_LOWER_SHOW = Setting('SIDEBAR_LOWER_SHOW', False, SIDEBAR_SET, dict(
+SIDEBAR_LOWER_SHOW = Setting('SIDEBAR_LOWER_SHOW', True, SIDEBAR_SET, dict(
 label = "Show Lower Block",
 help_text = "Check if your pages should display the lower sidebar block.",
 required=False))
@@ -30,7 +31,7 @@ SIDEBAR_LOWER_TEXT = Setting('SIDEBAR_LOWER_TEXT',
 u"""
 ## Learn more about OSQA
 
-The [**OSQA website**](http://www.osqa.net/) and [**OSQA wiki**](http://wiki.osqa.net/)
+The [**OSQA website**](http://www.osqa.net/) and [**OSQA wiki**](http://wiki.osqa.net/) \
 are also great resources to help you learn more about the OSQA open source Q&A system!
 """, SIDEBAR_SET, dict(
 label = "Lower Block Content",
