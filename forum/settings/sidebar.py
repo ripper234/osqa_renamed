4220f79214eb697103f50e6710e50e45662827fe
rick <rick@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-05-11 20:26:36 +0000

    Added missing WebFaction logo required by default sidebar promotion reference. Updated sidebar text to reference included logo.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@225 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum/settings/sidebar.py
+++ forum/settings/sidebar.py
@@ -11,12 +11,15 @@ required=False))
 
 SIDEBAR_UPPER_TEXT = Setting('SIDEBAR_UPPER_TEXT',
 u"""
-## [Reliable OSQA Hosting](http://www.webfaction.com?affiliate=osqa)
+[![WebFaction logo][2]][1]
+## [Reliable OSQA Hosting][1]
 
-We recommend [**WebFaction**](http://www.webfaction.com?affiliate=osqa) \
-for OSQA hosting. For under $10/month their reliable servers get the job done. See our \
+We recommend [**WebFaction**][1] for OSQA hosting. For \
+under $10/month their reliable servers get the job done. See our \
 [**step-by-step setup guide**](http://wiki.osqa.net/display/docs/Installing+OSQA+on+WebFaction).
-""", SIDEBAR_SET, dict(
+
+[1]: http://www.webfaction.com?affiliate=osqa
+[2]: /m/default/media/images/webfaction.png""", SIDEBAR_SET, dict(
 label = "Upper Block Content",
 help_text = " The upper sidebar block. ",
 widget=Textarea(attrs={'rows': '10'})))
