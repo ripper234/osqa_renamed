b029395233dcd4ca530b1bd40da05120061feefd
rick <rick@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-05-29 23:08:07 +0000

    Corrected spelling of "Wordpress" to "WordPress"
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@344 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum_modules/akismet/settings.py
+++ forum_modules/akismet/settings.py
@@ -4,8 +4,8 @@ from forum.settings.minrep import MIN_REP_SET
 from django.utils.translation import ugettext_lazy as _
 
 WORDPRESS_API_KEY = Setting('WORDPRESS_API_KEY', '', EXT_KEYS_SET, dict(
-label = _("Wordpress API key"),
-help_text = _("Your Wordpress API key. You can get one at <a href='http://wordpress.com/'>http://wordpress.com/</a>"),
+label = _("WordPress API key"),
+help_text = _("Your WordPress API key. You can get one at <a href='http://wordpress.com/'>http://wordpress.com/</a>"),
 required=False))
 
 REP_FOR_NO_SPAM_CHECK = Setting('REP_FOR_NO_SPAM_CHECK', 750, MIN_REP_SET, dict(
