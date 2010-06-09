c6731b0c562e6fe3c8fa58eca55d94595141115f
hernani <hernani@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-06-09 11:53:38 +0000

    Fixes bad english in userjoins action.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@398 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum/actions/user.py
+++ forum/actions/user.py
@@ -15,17 +15,18 @@ class UserJoinsAction(ActionProxy):
         send_template_email([self.user], "auth/email_validation.html", {'validation_code': hash})
 
     def describe(self, viewer=None):
-        return _("%(user)s as joined the %(app_name)s Q&A community") % {
-            'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
-            'app_name': APP_SHORT_NAME,
+        return _("%(user)s %(have_has)s joined the %(app_name)s Q&A community") % {
+        'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
+        'have_has': self.viewer_or_user_verb(viewer, self.user, _('have'), _('has')),
+        'app_name': APP_SHORT_NAME,
         }
 
 class EditProfileAction(ActionProxy):
     def describe(self, viewer=None):
         return _("%(user)s edited %(hes_or_your)s %(profile_link)s") % {
-            'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
-            'hes_or_your': self.viewer_or_user_verb(viewer, self.user, _('your'), _('his')),
-            'profile_link': self.hyperlink(self.user.get_profile_url(), _('profile')),
+        'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
+        'hes_or_your': self.viewer_or_user_verb(viewer, self.user, _('your'), _('his')),
+        'profile_link': self.hyperlink(self.user.get_profile_url(), _('profile')),
         }
 
 class BonusRepAction(ActionProxy):
@@ -34,17 +35,18 @@ class BonusRepAction(ActionProxy):
 
     def repute_users(self):
         self.repute(self.user, self._value)
-        self.user.message_set.create(message=_("Congratulations, you have been awarded an extra %s reputation points.") % self._value +
-                                     '<br />%s' % self.extra.get('message', _('Thank you')))
-        
+        self.user.message_set.create(
+                message=_("Congratulations, you have been awarded an extra %s reputation points.") % self._value +
+                '<br />%s' % self.extra.get('message', _('Thank you')))
+
     def describe(self, viewer=None):
         value = self.extra.get('value', _('unknown'))
         message = self.extra.get('message', '')
 
         return _("%(user)s %(was_were)s awarded %(value)s reputation points: %(message)s") % {
-            'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
-            'was_were': self.viewer_or_user_verb(viewer, self.user, _('were'), _('was')),
-            'value': value, 'message': message
+        'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
+        'was_were': self.viewer_or_user_verb(viewer, self.user, _('were'), _('was')),
+        'value': value, 'message': message
         }
 
 class AwardAction(ActionProxy):
@@ -73,8 +75,10 @@ class AwardAction(ActionProxy):
 
         self.user.save()
 
-        self.user.message_set.create(message=_("""Congratulations, you have received a badge '%(badge_name)s'. Check out <a href=\"%(profile_url)s\">your profile</a>.""") %
-                                     dict(badge_name=award.badge.name, profile_url=self.user.get_profile_url()))
+        self.user.message_set.create(message=_(
+                """Congratulations, you have received a badge '%(badge_name)s'. Check out <a href=\"%(profile_url)s\">your profile</a>."""
+                ) %
+        dict(badge_name=award.badge.name, profile_url=self.user.get_profile_url()))
 
     def cancel_action(self):
         award = self.award
@@ -95,7 +99,7 @@ class AwardAction(ActionProxy):
 
     def describe(self, viewer=None):
         return _("%(user)s %(were_was)s awarded the %(badge_name)s badge") % {
-            'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
-            'were_was': self.viewer_or_user_verb(viewer, self.user, _('were'), _('was')),
-            'badge_name': self.award.badge.name,
+        'user': self.hyperlink(self.user.get_profile_url(), self.friendly_username(viewer, self.user)),
+        'were_was': self.viewer_or_user_verb(viewer, self.user, _('were'), _('was')),
+        'badge_name': self.award.badge.name,
         }
