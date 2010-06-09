b3db8513b5d79ecc2b870c8dfaacb44aba3e1083
hernani <hernani@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-06-09 10:55:33 +0000

    Fixes OSQA 346, Error 500 when using a single quote (') in the search field or in the question title.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@397 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum_modules/pgfulltext/handlers.py
+++ forum_modules/pgfulltext/handlers.py
@@ -3,25 +3,27 @@ from django.db.models import Q
 from forum.models.question import Question, QuestionManager
 from forum.modules.decorators import decorate
 
+repl_re = re.compile(r"^'|[^\'\-_\s\w]|'$", re.UNICODE)
+sing_quote_re = re.compile(r"\'+")
+
 @decorate(QuestionManager.search, needs_origin=False)
 def question_search(self, keywords):
-    repl_re = re.compile(r"[^\'\-_\s\w]", re.UNICODE)
-    tsquery = " | ".join([k for k in repl_re.sub('', keywords).split(' ') if k])
+    tsquery = " | ".join([k for k in repl_re.sub('', sing_quote_re.sub("'", keywords.strip())).split(' ') if k])
     ilike = keywords + u"%%"
 
     return self.extra(
-                    tables = ['forum_rootnode_doc'],
-                    select={
-                        'ranking': """
+            tables = ['forum_rootnode_doc'],
+            select={
+            'ranking': """
                                 rank_exact_matches(ts_rank_cd('{0.1, 0.2, 0.8, 1.0}'::float4[], "forum_rootnode_doc"."document", to_tsquery('english', %s), 32))
                                 """,
-                    },
-                    where=["""
+            },
+            where=["""
                            "forum_rootnode_doc"."node_id" = "forum_node"."id" AND ("forum_rootnode_doc"."document" @@ to_tsquery('english', %s) OR
                            "forum_node"."title" ILIKE %s)
                            """],
-                    params=[tsquery, ilike],
-                    select_params=[tsquery],
-                    order_by=['-ranking']
-                )
+            params=[tsquery, ilike],
+            select_params=[tsquery],
+            order_by=['-ranking']
+            )
 
