f3c4f52c83a5c5f04b6bb83783769066610e5629
hernani <hernani@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-06-28 14:35:22 +0000

    Overrides two methods is django sitemap to prevent the example.com problem.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@459 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum/sitemap.py
+++ forum/sitemap.py
@@ -1,5 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from forum.models import Question
+from django.conf import settings
 
 class QuestionsSitemap(Sitemap):
     changefreq = 'daily'
@@ -12,3 +13,25 @@ class QuestionsSitemap(Sitemap):
 
     def location(self, obj):
         return obj.get_absolute_url()
+
+    def __get(self, name, obj, default=None):
+        try:
+            attr = getattr(self, name)
+        except AttributeError:
+            return default
+        if callable(attr):
+            return attr(obj)
+        return attr
+
+    def get_urls(self, page=1):
+        urls = []
+        for item in self.paginator.page(page).object_list:
+            loc = "http://%s%s" % (settings.APP_URL, self.__get('location', item))
+            url_info = {
+                'location':   loc,
+                'lastmod':    self.__get('lastmod', item, None),
+                'changefreq': self.__get('changefreq', item, None),
+                'priority':   self.__get('priority', item, None)
+            }
+            urls.append(url_info)
+        return urls    
