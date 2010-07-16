d9c06ef57483596ab196db15598a1fc487963815
matt <matt@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-07-16 20:17:40 +0000

    fixes OSQA-390 by adding a default sitemap.xml to the robots file.  This might not work completely with sub-directories, but there's a whole slew of issues to fix around that anyway.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@543 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum_modules/robotstxt/settings.py
+++ forum_modules/robotstxt/settings.py
@@ -1,10 +1,13 @@
 from forum.settings.base import Setting, SettingSet
 from django.forms.widgets import Textarea
+from django.core.urlresolvers import reverse
+from forum.settings import APP_URL
 
 ROBOTS_SET = SettingSet('robots', 'Robots txt', "Set up the robots.txt file.", 3000)
 
 ROBOTS_FILE = Setting('ROBOTS_FILE',
-"""
+"""Sitemap: %s/sitemap.xml
+
 User-Agent: *
 Disallow: /accounts/
 Disallow: /users/
@@ -12,7 +15,7 @@ Disallow: /revisions/
 Disallow: /search
 Disallow: /matching_tags
 Disallow: /*sort=
-""", ROBOTS_SET, dict(
+""" % APP_URL, ROBOTS_SET, dict(
 label = "Robots.txt file",
 help_text = """
 The robots.txt file search engine spiders will see.
