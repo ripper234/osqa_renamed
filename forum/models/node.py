49db7362d8a0e4050166b3b34999781749e6eef4
jordan <jordan@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2010-12-27 21:54:14 +0000

    Adding autolinking functionality, using mdx_urlize markdown extension.
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@635 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum/models/node.py
+++ forum/models/node.py
@@ -331,10 +331,10 @@ class Node(BaseModel, NodeContent):
     def create_revision(self, user, **kwargs):
         number = self.revisions.aggregate(last=models.Max('revision'))['last'] + 1
         revision = self._create_revision(user, number, **kwargs)
-        self.activate_revision(user, revision)
+        self.activate_revision(user, revision, extensions=['urlize'])
         return revision
 
-    def activate_revision(self, user, revision, *extensions):
+    def activate_revision(self, user, revision, extensions):
         self.title = revision.title
         self.tagnames = revision.tagnames
         self.body = self._as_markdown(revision.body, *extensions)
